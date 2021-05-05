6f90a233cd3f7267185ff1b363ee0c6e38314b58
unknown <jayeshsadhwani99@gmail.com>
2021-05-05 23:14:01 +0530

    Demo completed

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -11,6 +11,12 @@ class ChatScreenState extends State<ChatScreen> {
   final List<ChatMessage> _messages = <ChatMessage>[];
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 
   Widget _textComposerWidget() {
@@ -31,11 +37,12 @@ class ChatScreenState extends State<ChatScreen> {
               ),
             ),
             new Container(
-                margin: const EdgeInsets.symmetric(horizontal: 4.0),
-                child: new IconButton(
-                  icon: new Icon(Icons.send),
-                  onPressed: () => _handleSubmitted(_textController.text),
-                )),
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            ),
           ],
         ),
       ),
@@ -44,6 +51,26 @@ class ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return _textComposerWidget();
+    return new Column(
+      children: <Widget>[
+        new Flexible(
+          child: new ListView.builder(
+            padding: new EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int index) => _messages[index],
+            itemCount: _messages.length,
+          ),
+        ),
+        new Divider(
+          height: 1.0,
+        ),
+        new Container(
+          decoration: new BoxDecoration(
+            color: Theme.of(context).cardColor,
+          ),
+          child: _textComposerWidget(),
+        )
+      ],
+    );
   }
 }
